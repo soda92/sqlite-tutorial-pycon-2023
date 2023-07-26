9d36409a0cf2c8ee4f1703bd4e438edf16bd56c8
SodaCris <wang-jiaxun@outlook.com>
2023-07-26 18:14:21 +0800

    add pyinstaller support

--- ControlApp/app/main.py
+++ ControlApp/app/main.py
@@ -12,3 +12,7 @@ def main():
     window = AppWidget()
     window.show()
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
--- gui_app_w.pyw
+++ gui_app_w.pyw
@@ -1,7 +1,5 @@
 import os
 
-# print(os.environ.get("PYTHONPATH"))
-
 if __name__ == "__main__":
     from ControlApp.app.main import main
 
